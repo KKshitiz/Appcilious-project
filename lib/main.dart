0c14e421b5dedd9bf6307e61301e6acc4dedff51
kshitiz kamal <skilite007@gmail.com>
2020-09-28 13:39:39 +0530

    complete ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,270 @@
+import 'package:Appcilious/custom_clipper.dart';
 import 'package:flutter/material.dart';
+import 'constants.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _HomePageState extends State<HomePage> {
+  bool isConnected = false;
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      backgroundColor: kBgColor,
+      body: ListView(
+        children: [
+          Stack(
+            alignment: Alignment.topCenter,
+            clipBehavior: Clip.none,
+            children: [
+              upperCurvedContainer(context),
+              circularButtonWidget(context, screenWidth),
+            ],
+          ),
+          SizedBox(
+            height: screenWidth * 0.4,
+          ),
+          ConnectedStatusText(connectionStatus: isConnected),
+          SizedBox(
+            height: 20,
+          ),
+          locationCard(
+              'Random Location', Colors.transparent, kIndiaFlagUrl, 'India'),
+          SizedBox(
+            height: 20,
+          ),
+          locationCard(
+              'Select Location', Colors.indigo[100], kChinaFlagUrl, 'China'),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    );
+  }
+}
+
+Widget upperCurvedContainer(BuildContext context) {
+  return ClipPath(
+    clipper: MyCustomClipper(),
+    child: Container(
+      padding: EdgeInsets.symmetric(horizontal: 32, vertical: 36),
+      height: 320,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(gradient: kCurveGradient),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _topRow(),
+          Text(
+            'Satellite VPN',
+            style: kVpnStyle,
+          ),
+          _bottomRow(),
+        ],
+      ),
+    ),
+  );
+}
+
+Widget _topRow() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      Container(
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+        height: 50,
+        decoration: BoxDecoration(
+          color: kBgColor,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Row(
+          children: [
+            Icon(
+              Icons.vpn_key,
+              color: Colors.white,
+            ),
+            SizedBox(
+              width: 12,
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              'Go Premium',
+              style: TextStyle(color: Colors.white),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      Container(
+        height: 50,
+        width: 50,
+        decoration: BoxDecoration(
+          color: kBgColor,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Center(
+          child: Icon(
+            Icons.settings,
+            size: 26,
+            color: Colors.white,
+          ),
+        ),
+      )
+    ],
+  );
+}
+
+Widget _bottomRow() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      Text(
+        'Upload\n546 kB\'s',
+        style: kSpeedStyle,
+      ),
+      Text(
+        'Download\n32 mb/s',
+        style: kSpeedStyle,
+      ),
+    ],
+  );
+}
+
+class ConnectedStatusText extends StatelessWidget {
+  final bool connectionStatus;
+  ConnectedStatusText({this.connectionStatus});
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: RichText(
+        textAlign: TextAlign.center,
+        text: TextSpan(
+          text: 'Status :',
+          style: kConnectedStyle,
+          children: [
+            TextSpan(text: ' connected\n', style: kConnectedGreenStyle),
+            TextSpan(text: '00:22:45', style: kConnectedSubtitleStyle),
+          ],
+        ),
+      ),
     );
   }
+}
+
+Widget circularButtonWidget(BuildContext context, width) {
+  return Positioned(
+    bottom: -width * 0.35,
+    child: Stack(
+      alignment: Alignment.center,
+      children: [
+        Container(
+          height: width * 0.51,
+          width: width * 0.51,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            gradient: kCurveGradient,
+          ),
+          child: Center(
+            child: Container(
+              height: width * 0.4,
+              width: width * 0.4,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: kBgColor,
+              ),
+              child: Center(
+                child: Container(
+                  height: width * 0.3,
+                  width: width * 0.3,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    gradient: kGreenGradient,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color(0xFF00D58D).withOpacity(0.2),
+                        spreadRadius: 15,
+                        blurRadius: 15,
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: Icon(
+                      Icons.wifi_lock,
+                      color: Colors.white,
+                      size: 50,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
+    ),
+  );
+}
+
+Widget locationCard(title, cardBgColor, flag, country) {
+  return Container(
+    margin: EdgeInsets.symmetric(vertical: 8, horizontal: 32),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: kLocationTitleStyle,
+        ),
+        SizedBox(
+          height: 14,
+        ),
+        Container(
+          height: 80,
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: cardBgColor,
+            border: Border.all(
+                color: Color(0xFF9BB1BD), style: BorderStyle.solid, width: 0.5),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Center(
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.transparent,
+                radius: 20,
+                backgroundImage: NetworkImage(flag),
+              ),
+              title: Text(
+                country,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600),
+              ),
+              trailing: Icon(
+                Icons.refresh,
+                size: 28,
+                color: Colors.white70,
+              ),
+            ),
+          ),
+        )
+      ],
+    ),
+  );
 }
